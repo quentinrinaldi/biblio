5a4e7a283e6d8dc79e024820c6fd6422d24535d8
quentinrinaldi <quentin.rinaldi@gmail.com>
2020-11-16 21:48:26 +0100

    changing bootstrap to native in progress

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,7 @@
 // any CSS you import will output into a single css file (app.css in this case)
 
 //import "./styles/app.scss";
-import "./styles/app.css";
+import "./styles/app.scss";
 import "./css/shared/base.css";
 
 var $ = require("jquery");
