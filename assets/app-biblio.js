d4989422cce35f8c11e1226553612bdd98331d33
quentinrinaldi <quentin.rinaldi@gmail.com>
2020-11-17 16:00:24 +0100

    book details ended

--- assets/app-biblio.js
+++ assets/app-biblio.js
@@ -1,4 +1,4 @@
-import "./css/biblio/base.css";
+//import "./css/biblio/base.css";
 import "./css/biblio/homepage.css";
 import "./css/shared/homepage.css";
-import "./css/book-show.css";
+//import "./css/book-show.css";
